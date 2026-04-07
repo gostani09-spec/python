e4a2e449311284abd2b4ad8e2e6a48ade274969d
gostani09-spec <gostani09@gmail.com>
2026-04-07 14:57:11 +0530

    Add files via upload

---
+++ classcse.py
@@ -0,0 +1,11 @@
+class CSE:
+    attr1="pravalli"
+    attr2="keers"
+    attr3="varsh"
+    def fun(self):
+        print("this is",self.attr1)
+        print("this is",self.attr2)
+        print("this is",self.attr3)
+AUS=CSE()
+print(AUS.attr1)
+AUS.fun()
---
+++ classdog.py
@@ -0,0 +1,9 @@
+class dog:
+    attr1="lyca"
+    attr2="ruby"
+    def fun(self):
+        print("this is",self.attr1)
+        print("this is",self.attr2)
+lussy=dog()
+print(lussy.attr1)
+lussy.fun()
